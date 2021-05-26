c31d8a0cfec0cfc918d9ff1c1938fb1348f5827d
unknown <kachi.madu234@gmail.com>
2021-05-26 21:36:12 +0100

    Nav bar design update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from "./components/Navbar.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <h2>Portfolio Website...</h2>
+    <Navbar />
   );
 }
 
