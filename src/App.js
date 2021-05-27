3a64ff626618842f0332f069431772db18e1e379
unknown <kachi.madu234@gmail.com>
2021-05-27 13:09:54 +0100

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from "./components/Navbar.js";
+//install and import bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,38 @@
 import React from 'react'
+//import logo
 import logo from '../logoPersonal.png'
 
 const Navbar = () => {
     return (
+        //navbar code from bootstrap website
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#"><img className='logo' src={logo} alt="logo..."/></a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
+            <div className="container">
+                <a className="navbar-brand" href="#"><img className='logo' src={logo} alt="logo..."/></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav ml-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Link</a>
-      </li>     
-    </ul>   
-  </div>
-</nav>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">about me</a>
+                        </li>     
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">work style</a>
+                        </li>    
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">portfolio</a>
+                        </li>    
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">contact</a>
+                        </li>    
+                    </ul>   
+                </div>
+            </div>
+        </nav>
     )
 }
 
